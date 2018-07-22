961ddb228100ef00e0d02260c38547fe67f4a2c4
Kevin Kelly <kvinklly@gmail.com>
2018-07-22 13:53:22 -0500

    added devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users, path: 'auth', path_names: { 
+    sign_in: 'login', sign_out: 'logout', registration: 'register', sign_up: 'signup'
+  }
+  get 'home/index'
+  root 'home#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
