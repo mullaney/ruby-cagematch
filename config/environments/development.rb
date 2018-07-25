7d5af60da855d2d69715a62c0069c68572b06ac5
Kevin Kelly <kvinklly@gmail.com>
2018-07-25 13:04:34 -0500

    cross origin resource allowed

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -53,4 +53,10 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # For cross origin resource sharing in dev environment
+  config.action_dispatch.default_headers = {
+    'Access-Control-Allow-Origin' => 'http://localhost:3006',
+    'Access-Control-Request-Method' => %w{GET}.join(",")
+  }
 end
