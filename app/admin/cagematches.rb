03c343138bc4e93bdb7e0b39ceec5980b3030132
Kevin Kelly <kvinklly@gmail.com>
2018-09-03 11:15:43 -0500

    add permit_params for Cagematch

--- app/admin/cagematches.rb
+++ app/admin/cagematches.rb
@@ -1,4 +1,9 @@
 ActiveAdmin.register Cagematch do
+  permit_params :title, :slug, :time_slot, :theater_name, :theater_url, 
+                :venue_address, :box_office_url, :box_office_phone_number, 
+                :ticket_price_description, :show_description, :logo_url, 
+                :show_picture_url, :tag_line
+
   filter :title
   filter :slug
   filter :theater_name
